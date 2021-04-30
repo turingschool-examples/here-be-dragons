70169399332e2716fc86f383fb82f4cb2590d027
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-04-30 16:16:41 -0600

    make test 3 pass

--- test/nytimes_test.rb
+++ test/nytimes_test.rb
@@ -31,6 +31,9 @@ class NytimesTest < Minitest::Test
   def test_it_can_get_all_stories_with_subsection_of_politics
     #Set your code to the local variable, "result"
 
+    result = @hash[:results].select do |element|
+      element[:subsection] == "Politics"
+    end
 
     assert result.is_a? (Array)
     assert_equal 6, result.count
